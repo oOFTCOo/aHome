6f599efdb9ad15094c2299859161d8b3161e9765
Murmann <john@doe.com>
2013-11-15 22:30:05 +0100

    Added /src/.../DeviceParser.java: Determines whether recognized commands can be resolved properly.

---
+++ src/com/example/modernhome/CommandParser.java
@@ -0,0 +1,73 @@
+package com.example.modernhome;
+
+import java.util.ArrayList;
+
+/**
+ * Resolves recognized command words.
+ * @author pm
+ */
+public class CommandParser {
+	private static boolean TO_LOWER_CASE = true;
+	private DeviceParser parser;
+	private ArrayList<String> groups;
+
+	public CommandParser(DeviceParser config) {
+		parser = config;
+		groups = parser.getGroupNames();
+	}
+
+	/**
+	 * Determines whether a tuple of a certain location, device, and status combination exists. 
+	 * @param location	Literal location.
+	 * @param device	Literal device.
+	 * @param status	Literal status.
+	 * @return	Whether or not the combination of the specified arguments exists.
+	 */
+	public boolean existsLocationDeviceStatus(String location, String device, String status) {
+		if(TO_LOWER_CASE) {
+			location = location.toLowerCase();
+			device = device.toLowerCase();
+			status = status.toLowerCase();
+		}
+		
+		if(! groups.contains(location))
+			return false;
+
+		// Traverse devices of specified location, i. e. group.
+		// We assume that the group attribute equals the location.
+		ArrayList<String> devices = parser.getDevicesOfGroup(location);
+		boolean found = false;
+		String deviceId = null;
+		for(String item: devices) {
+			try {
+				String name = parser.getDeviceNameById(item);
+				if(name.equals(device)) {
+					found = true;
+					deviceId = item;
+				}
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+		if(! found)
+			return false;
+
+		// Check states of device
+		ArrayList<String> states = parser.getStatesOfDevice(deviceId);
+		for(String item: states) {
+			try {
+				String name = parser.getStateName(item);
+				if(name.equals(status)) {
+					return true;
+				}
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+
+		// Combination not found.
+		return false;
+	}
+}
